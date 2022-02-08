c7cf90aa13f6baa6dc396a745887c907868f8e6a
imparpaing <saintsikorski@gmail.com>
2022-02-08 21:06:55 +0100

    Started working on Python implementation

---
+++ main.py
@@ -0,0 +1,28 @@
+print('Hello')
+
+storeUserName = input('Enter you name: ')
+
+print('\nWelcome, ' + storeUserName)
+print('What would you like to do?\n')
+
+# std::cout << "[ 1] Display tasks\n";
+# std::cout << "[ 2] Write to file\n";
+# std::cout << "[ 3] Exit program\n";
+print('[ 1] Display tasks')
+print('[ 2] Write to file')
+print('[ 3] Exit')
+print('\n')
+
+storeUserSelection = input('Selection: ')
+
+if (storeUserSelection != '1' and storeUserSelection != '2' and storeUserSelection != '3'):
+    print('Incorrect input')
+
+elif storeUserSelection == '1':
+    print('One')
+
+elif storeUserSelection == '2':
+    print('Two')
+
+elif storeUserSelection == '3':
+    print('Three')
