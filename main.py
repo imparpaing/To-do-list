a5dc1814f25cdd8e0eaa174a3faa5bbfc05dcf76
imparpaing <saintsikorski@gmail.com>
2022-02-09 18:06:38 +0100

    Added username creation

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-print('Hello')
+from os.path import exists
 
-storeUserName = input('Enter you name: ')
+file_exists = exists('userName.txt')
 
 # WELCOME SCREEN
-print('\nWelcome, ' + storeUserName)
-print('What would you like to do?\n')
+if file_exists == False:
+    print('Hello')
+    storeUserName = input('Enter your name: ')
 
+    f = open('userName.txt', 'w')
+    f.write(storeUserName)
+
+    with open('userName.txt', 'r') as f:
+        storeUserName = f.read().rstrip()
+        
+    print('\nWelcome, ' + str(storeUserName))
+    print('What would you like to do?\n')
+
+    f.close()
+
+# IF 'USERNAME.TXT' FILE WAS FOUND SKIP CREATING THE USERNAME
+else:
+    with open('userName.txt', 'r') as f:
+        storeUserName = f.read().rstrip()
+
+    print('\nWelcome, ' + str(storeUserName))
+    print('What would you like to do?\n')
+    f.close()
+    
 # MENU
 print('[ 1] Display tasks')
 print('[ 2] Write to file')
@@ -14,14 +35,13 @@ print('[ 4] Exit')
 print('\n')
 
 storeUserSelection = input('Selection: ')
-print('\n')
 
 # TASK SELECTION
 if (storeUserSelection != '1' and storeUserSelection != '2' and storeUserSelection != '3' and storeUserSelection != '4'):
     print('Incorrect input')
 
 elif storeUserSelection == '1':
-    print('SELECTED => [Read from file]')
+    print('SELECTED => [Read from file]\n')
     print('TASKS:\n')
     f = open('tasks.txt', 'r')
     print(f.read())
@@ -30,12 +50,12 @@ elif storeUserSelection == '2':
     print('SELECTED => [Write to file]\n')
     f = open('tasks.txt', 'a')
     writeToFile = input('Input: ')
-    f.write(writeToFile)
-    f.write('\n')
+    f.write(writeToFile + '\n')
     print('Writing to file...')
     f.close()
 
 elif storeUserSelection == '3':
+    print('SELECTED => [Clear file]\n')
     f = open('tasks.txt', 'w')
     f.write('')
     print('Clearing file...')
